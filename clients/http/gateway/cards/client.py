e89bd4c05bd9f20965f3ae58f0de487066da9de7
cathoFrancis <catho.francis@gmai.com>
2025-08-30 12:53:57 +0300

    Практикуемся в реализации HTTP API клиентов

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -1,7 +1,24 @@
-from httpx import Response
+from httpx import Response, request
 from typing import TypedDict
 
 from clients.http.client import HTTPClient
+from clients.http.gateway.client import build_gateway_http_client
+
+
+class CardDict(TypedDict):
+    """
+    Описание структуры карты.
+    """
+    id: str
+    pin: str
+    cvv: str
+    type: str
+    status: str
+    accountId: str
+    cardNumber: str
+    cardHolder: str
+    expiryDate: str
+    paymentSystem: str
 
 
 class IssueVirtualCardRequestDict(TypedDict):
@@ -12,12 +29,26 @@ class IssueVirtualCardRequestDict(TypedDict):
     accountId: str
 
 
+class IssueVirtualCardResponseDict(TypedDict):
+    """
+    Описание структуры ответа выпуска виртуальной карты.
+    """
+    card: CardDict
+
+
 class IssuePhysicalCardRequestDict(TypedDict):
     """
     Структура данных для выпуска физической карты.
     """
     userId: str
     accountId: str
+
+
+class IssuePhysicalCardResponseDict(TypedDict):
+    """
+    Описание структуры ответа выпуска физической карты.
+    """
+    card: CardDict
 
 
 class CardsGatewayHTTPClient(HTTPClient):
@@ -42,3 +73,23 @@ class CardsGatewayHTTPClient(HTTPClient):
         :return: Ответ от сервера (объект httpx.Response).
         """
         return self.post("/api/v1/cards/issue-physical-card", json=request)
+
+    def issue_virtual_card(self, user_id: str, account_id: str) ->IssueVirtualCardResponseDict:
+        request = IssueVirtualCardRequestDict(userId=user_id, accountId=account_id)
+        response = self.issue_physical_card_api(request)
+        return response.json()
+
+    def issue_physical_card(self, user_id: str, account_id: str) -> IssuePhysicalCardResponseDict:
+        request = IssuePhysicalCardRequestDict(userId=user_id, accountId=account_id)
+        response = self.issue_physical_card_api(request)
+        return response.json()
+
+
+# Добавляем builder для CardsGatewayHTTPClient
+def build_cards_gateway_http_client() -> CardsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр CardsGatewayHTTPClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию CardsGatewayHTTPClient.
+    """
+    return CardsGatewayHTTPClient(client=build_gateway_http_client())
